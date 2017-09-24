2c4a9f45374580c8dedb7211764dba2f87f6a5b8
Matt Gallagher <matt@zqueue.com>
2017-09-24 10:02:43 +1000

    Implemented unseparated.

---
+++ CwlWorstPossibleApplication/GameViewController.swift
@@ -0,0 +1,170 @@
+//
+//  GameViewController.swift
+//  CwlWorstPossibleApplication
+//
+//  Created by Matt Gallagher on 2017/09/01.
+//  Copyright © 2017 Matt Gallagher. All rights reserved.
+//
+
+import UIKit
+import GameKit
+
+class GameViewController: UIViewController {
+	static let gameWidth: Int = 10
+	static let gameHeight: Int = 10
+	static let initialMineCount: Int = 15
+	
+	@IBOutlet var squaresToClear: UILabel?
+	@IBOutlet var flagMode: UISwitch?
+	@IBOutlet var newGameButton: UIButton?
+	
+	var squares: Array<SquareView> = []
+	var nonMineSquaresRemaining = 0 { didSet {
+		if nonMineSquaresRemaining == -1 {
+			squaresToClear?.text = NSLocalizedString("Boom... you lose!", comment: "")
+		} else if nonMineSquaresRemaining == 0 {
+			squaresToClear?.text = NSLocalizedString("None... you win!", comment: "")
+		} else {
+			squaresToClear?.text = "\(nonMineSquaresRemaining)"
+		}
+	} }
+	
+	func loadGame(newSquares: Array<SquareView>, remaining: Int) {
+		squares.forEach { $0.removeFromSuperview() }
+		squares = newSquares
+		nonMineSquaresRemaining = remaining
+		for s in squares {
+			self.view.addSubview(s)
+			s.addTarget(self, action: #selector(squareTapped(_:)), for: .primaryActionTriggered)
+		}
+	}
+	
+	@IBAction func startNewGame() {
+		loadGame(newSquares: newMineField(mineCount: GameViewController.initialMineCount), remaining: GameViewController.gameWidth * GameViewController.gameHeight - GameViewController.initialMineCount)
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		newGameButton?.layer.cornerRadius = 8
+		startNewGame()
+	}
+	
+	@objc func squareTapped(_ sender: Any?) {
+		guard let s = sender as? SquareView, nonMineSquaresRemaining > 0 else { return }
+		
+		if flagMode?.isOn == true, s.covering != .uncovered {
+			s.covering = s.covering == .covered ? .flagged : .covered
+			return
+		} else if s.covering == .flagged {
+			return
+		}
+		
+		if s.isMine {
+			s.covering = .uncovered
+			nonMineSquaresRemaining = -1
+			return
+		}
+		
+		nonMineSquaresRemaining -= uncover(squares: &squares, index: s.location)
+	}
+	
+	override func viewDidLayoutSubviews() {
+		let availableWidth = self.view.frame.size.width
+		let usedWidth = CGFloat(SquareView.squareSize + 2) * CGFloat(GameViewController.gameWidth)
+		let availableHeight = self.view.frame.size.height
+		let usedHeight = CGFloat(SquareView.squareSize + 2) * CGFloat(GameViewController.gameHeight)
+		for y in 0..<GameViewController.gameHeight {
+			for x in 0..<GameViewController.gameWidth {
+				let s = squares[x + y * GameViewController.gameWidth]
+				s.frame.origin = CGPoint(x: 0.5 * (availableWidth - usedWidth) + CGFloat(x) * CGFloat(SquareView.squareSize + 2) + 1, y: 0.5 * (availableHeight - usedHeight) + CGFloat(y) * CGFloat(SquareView.squareSize + 2) + 1)
+			}
+		}
+	}
+	
+	override func decodeRestorableState(with coder: NSCoder) {
+		super.decodeRestorableState(with: coder)
+		if coder.containsValue(forKey: String.remainingKey) {
+			flagMode?.isOn = coder.decodeBool(forKey: String.flagModeKey)
+		}
+		
+		if let squaresArray = coder.decodeObject(forKey: String.squaresKey) as? Array<Dictionary<String, Any>>, coder.containsValue(forKey: String.remainingKey) {
+			do {
+				let newSquares = try squaresArray.map { try SquareView(fromDictionary: $0) }
+				loadGame(newSquares: newSquares, remaining: coder.decodeInteger(forKey: String.remainingKey))
+			} catch {
+				startNewGame()
+			}
+		}
+	}
+	
+	override func encodeRestorableState(with coder: NSCoder) {
+		super.encodeRestorableState(with: coder)
+		coder.encode(squares.map { $0.toDictionary() } as Array<Dictionary<String, Any>>, forKey: String.squaresKey)
+		coder.encode(nonMineSquaresRemaining as Int, forKey: String.remainingKey)
+		coder.encode((flagMode?.isOn == true) as Bool, forKey: String.flagModeKey)
+	}
+}
+
+fileprivate extension String {
+	static let squaresKey = "squares"
+	static let remainingKey = "remaining"
+	static let flagModeKey = "flagMode"
+}
+
+func newMineField(mineCount: Int) -> Array<SquareView> {
+	let random = GKRandomDistribution(randomSource: GKRandomSource(), lowestValue: 0, highestValue: GameViewController.gameWidth * GameViewController.gameHeight - 1)
+	var squares = Array<SquareView>()
+	for l in 0..<(GameViewController.gameWidth * GameViewController.gameHeight) {
+		squares.append(SquareView(location: l))
+	}
+	
+	for _ in 1...mineCount {
+		var n = 0
+		repeat {
+			n = random.nextInt()
+		} while squares[n].isMine
+		squares[n].isMine = true
+		iterateAdjacent(squares: &squares, index: n) { (ss: inout Array<SquareView>, index: Int) in
+			if !ss[index].isMine {
+				ss[index].adjacent += 1
+			}
+		}
+	}
+	return squares
+}
+
+func uncover(squares: inout Array<SquareView>, index: Int) -> Int {
+	guard squares[index].covering == .covered else { return 0 }
+	
+	squares[index].covering = .uncovered
+	
+	if squares[index].adjacent == 0 {
+		var cleared = 1
+		iterateAdjacent(squares: &squares, index: index) { (ss: inout Array<SquareView>, i: Int) in
+			cleared += uncover(squares: &ss, index: i)
+		}
+		return cleared
+	} else {
+		return 1
+	}
+}
+
+func iterateAdjacent(squares: inout Array<SquareView>, index n: Int, process: (inout Array<SquareView>, Int) -> ()) {
+	let isOnLeftEdge = n % GameViewController.gameWidth == 0
+	let isOnRightEdge = n % GameViewController.gameWidth == GameViewController.gameHeight - 1
+	
+	if n >= GameViewController.gameWidth {
+		if !isOnLeftEdge { process(&squares, n - GameViewController.gameWidth - 1) }
+		process(&squares, n - GameViewController.gameWidth)
+		if !isOnRightEdge { process(&squares, n - GameViewController.gameWidth + 1) }
+	}
+	
+	if !isOnLeftEdge { process(&squares, n - 1) }
+	if !isOnRightEdge { process(&squares, n + 1) }
+	
+	if n < GameViewController.gameWidth * (GameViewController.gameHeight - 1) {
+		if !isOnLeftEdge { process(&squares, n + GameViewController.gameWidth - 1) }
+		process(&squares, n + GameViewController.gameWidth)
+		if !isOnRightEdge { process(&squares, n + GameViewController.gameWidth + 1) }
+	}
+}
---
+++ CwlWorstPossibleApplication/SquareView.swift
@@ -0,0 +1,83 @@
+//
+//  SquareView.swift
+//  CwlWorstPossibleApplication
+//
+//  Created by Matt Gallagher on 2017/09/20.
+//  Copyright © 2017 Matt Gallagher. All rights reserved.
+//
+
+import UIKit
+
+class SquareView: UIButton {
+	static let font = UIFont.systemFont(ofSize: 18)
+	static let squareSize: CGFloat = 30
+	static let textPadding: CGFloat = 2
+	
+	enum Covering: String {
+		case uncovered = "uncovered"
+		case covered = "covered"
+		case flagged = "flagged"
+	}
+
+	var covering: Covering = .covered { didSet { setNeedsDisplay() } }
+	var isMine: Bool = false
+	var adjacent: Int8 = 0
+	var location: Int
+	
+	init(location: Int) {
+		self.location = location
+		super.init(frame: CGRect(x: 0, y: 0, width: SquareView.squareSize, height: SquareView.squareSize))
+	}
+	
+	init(fromDictionary dictionary: Dictionary<String, Any>) throws {
+		if let coveringString = dictionary[String.coveringKey] as? String, let covering = SquareView.Covering(rawValue: coveringString), let location = dictionary[String.locationKey] as? Int, let adjacent = dictionary[String.adjacentKey] as? Int8, let isMine = dictionary[String.isMineKey] as? Bool {
+			self.adjacent = adjacent
+			self.covering = covering
+			self.isMine = isMine
+			self.location = location
+			super.init(frame: CGRect(x: 0, y: 0, width: SquareView.squareSize, height: SquareView.squareSize))
+		} else {
+			throw NSError(domain: NSCocoaErrorDomain, code: CocoaError.coderValueNotFound.rawValue, userInfo: nil)
+		}
+	}
+	
+	required init(coder: NSCoder) { fatalError() }
+	
+	override func draw(_ rect: CGRect) {
+		let path = UIBezierPath(rect: rect)
+		if covering == .uncovered {
+			UIColor(red: 0.8, green: 0.68, blue: 0.6, alpha: 1).set()
+		} else {
+			UIColor(red: 0.472, green: 0.333, blue: 0.277, alpha: 1).set()
+		}
+		path.fill()
+		
+		if covering != .covered {
+			let alignCenter = NSMutableParagraphStyle()
+			alignCenter.alignment = .center
+			alignCenter.minimumLineHeight = SquareView.squareSize - 2 * SquareView.textPadding
+			let string: String
+			if covering == .flagged {
+				string = "⚑"
+			} else if isMine {
+				string = "💣"
+			} else if adjacent != 0 {
+				string = "\(adjacent)"
+			} else {
+				string = ""
+			}
+			NSAttributedString(string: string, attributes: [.paragraphStyle: alignCenter, .font: SquareView.font]).draw(in: rect)
+		}
+	}
+	
+	func toDictionary() -> Dictionary<String, Any> {
+		return [String.coveringKey: covering.rawValue, String.isMineKey: isMine, String.adjacentKey: adjacent, String.locationKey: location]
+	}
+}
+
+fileprivate extension String {
+	static let coveringKey = "covering"
+	static let isMineKey = "isMine"
+	static let adjacentKey = "adjacent"
+	static let locationKey = "location"
+}
